d01988329a511fbda7a5f0c9e3894447e9fe0012
unknown <Patrick@Conrad.(none)>
2012-07-14 14:10:26 -0400

    removal of class files

--- src/com/cs446/ComicCruiser/ComicRepository/RepositoryFacade.java
+++ src/com/cs446/ComicCruiser/ComicRepository/RepositoryFacade.java
@@ -10,6 +10,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.Vector;
 
 import com.cs446.ComicCruiser.Activity.ComicCruiserHomeActivity;
@@ -25,8 +27,12 @@ public class RepositoryFacade {
 	
 	public static final String PREFERENCE_ISSUE_LIST = "AppIssueList";
 
+
+	private static final int RECENTLY_READ_ISSUE_LIMIT = 3;
+
 	
-	static Vector<Issue> issueList = new Vector<Issue>();
+	static Vector<Issue> issueList;
+	static LimitedQueue<Issue> recentlyRead;
 	
 	public static Issue getIssueById(String id){
 		return null;
@@ -47,13 +53,16 @@ public class RepositoryFacade {
 	}
 	@SuppressWarnings("unchecked")
 	public static void initializeRepository(){
-		issueList = new Vector<Issue>();
 		
 		//read up issue objects
 		Gson gson = new Gson();
 		SharedPreferences settings = ComicCruiserHomeActivity.getInstance().getSharedPreferences("ComicCruiserPreferences", Activity.MODE_PRIVATE); 
 		Type issueListType = new TypeToken<Vector<Issue>>(){}.getType();
 		issueList = gson.fromJson(settings.getString(PREFERENCE_ISSUE_LIST, null), issueListType);
+		if(issueList == null){
+			issueList = new Vector<Issue>();
+		}
+		recentlyRead = new LimitedQueue<Issue>(RepositoryFacade.RECENTLY_READ_ISSUE_LIMIT);
 	}
 	
 	public static void persistRepository(){
@@ -76,7 +85,7 @@ public class RepositoryFacade {
 	public static ArrayList<String> getRecentIssues() {
 		//TODO: limit number and make them most recently read issues
 		ArrayList<String> values = new ArrayList<String>();
-		for(Issue issue : issueList){
+		for(Issue issue : recentlyRead){
 			values.add(issue.getTitle());
 		}
 		return values;
@@ -92,12 +101,29 @@ public class RepositoryFacade {
 	
 	public static ImageIterator openIssueForReading(Issue issue){
 		//move to front of queue
+		recentlyRead.add(issue);
 		//get image iterator
 		return new PageIterator(issue);
 	}
 	
 	public static void closeIssue(Issue issue){
 		//recycle bitmaps
+		//set bootmark?
 	}
 	
+	public static class LimitedQueue<E> extends LinkedList<E> {
+
+	    private int limit;
+
+	    public LimitedQueue(int limit) {
+	        this.limit = limit;
+	    }
+
+	    @Override
+	    public boolean add(E o) {
+	        super.add(o);
+	        while (size() > limit) { super.remove(); }
+	        return true;
+	    }
+	}
 }
